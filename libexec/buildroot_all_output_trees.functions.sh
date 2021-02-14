1ce83e5914e4690c6d50c0cadcb9a5a67ddbc702
Stephen Rogers <inbox.c7r@steve-rogers.com>
2021-02-14 18:09:44 -0500

    Break out and generalize `best_defconfig_fbn_hint_for()`
    The search for a defconfig hint now considers .config files from
    existing buildroot output trees, in an appropriate order.

--- libexec/buildroot_all_output_trees.functions.sh
+++ libexec/buildroot_all_output_trees.functions.sh
@@ -1,6 +1,6 @@
 ##/bin/bash
 ## Provides function buildroot_all_output_trees() and friends.
-## 
+##
 
 [ -z "$buildroot_all_output_trees_functions_p" ] || return 0
 
@@ -49,44 +49,93 @@ function check_looks_like_buildroot_xctc_defconfig() { # xctc_defconfig_fbn
 	check_looks_like_buildroot_defconfig_for xctc "$@"
 }
 
+function best_defconfig_fbn_hint_for() {( # output_selector [ defconfig_fbn_hint ]
+
+	local output_selector=${1:?missing value for output_selector} ; shift 1
+
+	local defconfig_fbn_hint=${1} ; shift 1 || :
+
+	local config_file_selectors=()
+	local config_file_selector
+
+	case "${output_selector:?}" in
+	main)
+		config_file_selectors+=( main xctc )
+		;;
+	xctc)
+		config_file_selectors+=( xctc main )
+		;;
+	*|'')
+		echo 1>&2 "${FUNCNAME:?}: unrecognized output selector: ${output_selector}"
+		return 2
+	esac
+
+	local result=
+
+	load_buildroot_config --defaults file-only --output-none
+
+	# don't build the toolchain/sdk implicitly unless you've done so already
+
+	if [[ ${output_selector:?} == xctc && ! -d ${BR2_OUTPUT_XCTC_DIR:?} ]] ; then
+
+		result=:skip:${output_selector:?}
+	else
+		result=${defconfig_fbn_hint}
+	fi
+
+	if ! [[ -n ${result} ]] ; then
+
+		# find the most appropriate buildroot .config file, and rely on that
+
+		for config_file_selector in "${config_file_selectors[@]}" ; do
+
+			load_buildroot_config --defaults file-omit --output-${config_file_selector}
+
+			result=${BR2_DEFCONFIG:-}
+
+			! [[ -n ${result} ]] || break
+		done
+	fi
+
+	if [[ -n ${result} ]] ; then
+
+		result=$(basename "${result}")
+
+		result=${result/main_defconfig/${output_selector:?}_defconfig}
+		result=${result/xctc_defconfig/${output_selector:?}_defconfig}
+	fi
+
+	echo "${result}"
+)}
+
 function resolve_buildroot_defconfig_for() { # output_selector defconfig_fbn [ defconfig_fbn_hint ]
 
 	local output_selector=${1:?missing value for output_selector} ; shift 1
 
 	local defconfig_fbn=${1:?missing value for defconfig_fbn} ; shift 1
 
-	if [[ ${defconfig_fbn:?} =~ ^(:skip:${output_selector:?})$ ]] ; then
-
-		return 0
-	fi
-
-	local result=
-
 	local defconfig_fbn_hint=${1} ; shift 1 || :
 
-	if [[ ${defconfig_fbn:?} =~ ^(:infer:${output_selector:?})$ ]] ; then
+	##
 
-		result=$(basename "${defconfig_fbn_hint:-${BR2_DEFCONFIG:-${BR2_ENV_DEFCONFIG:-}}")
+	local result=${defconfig_fbn}
 
-		result=${result/main_defconfig/${output_selector:?}_defconfig}
-		result=${result/xctc_defconfig/${output_selector:?}_defconfig}
+	if [[ ${result:?} =~ ^(:infer:${output_selector:?})$ ]] ; then
 
-		if [[ ${output_selector} == xctc ]] ; then
-
-			# accept inference only if selected build output already exists
-
-			eval local selected_build_output_dpn=\${BR2_ENV_OUTPUT_${output_selector^^?}_DIR}
-
-			if ! [[ -n ${selected_build_output_dpn} && -d ${selected_build_output_dpn} ]] ; then 
-
-				result=
-			fi
-		fi
-	else
-		result=$(basename "${defconfig_fbn:?}")
+		result=$(best_defconfig_fbn_hint_for "${output_selector:?}" "${defconfig_fbn_hint}")
 	fi
 
-	check_looks_like_buildroot_defconfig_for "${output_selector:?}" "${result:?}" || return $?
+	if [[ ${result:?} =~ ^(:skip:${output_selector:?})$ ]] ; then
+
+		result=
+	fi
+
+	if [[ -n ${result} ]] ; then
+
+		result=$(basename "${result}")
+
+		check_looks_like_buildroot_defconfig_for "${output_selector:?}" "${result:?}" || return $?
+	fi
 
 	echo "${result}"
 }
@@ -166,19 +215,11 @@ function buildroot_all_output_trees_build() { # [ main_defconfig_fbn [ xctc_defc
 
 	local main_defconfig_fbn= xctc_defconfig_fbn=
 
-	if [[ ${#} -ge 1 ]] ; then
+	main_defconfig_fbn=${1:-:infer:main} ; shift 1 || :
+	main_defconfig_fbn=$(resolve_buildroot_main_defconfig "${main_defconfig_fbn:?}")
 
-		main_defconfig_fbn=${1:-:infer:main} ; shift 1
-
-		main_defconfig_fbn=$(resolve_buildroot_main_defconfig "${main_defconfig_fbn:?}")
-	fi
-
-	if [[ ${#} -ge 1 ]] ; then
-
-		xctc_defconfig_fbn=${1:-:infer:xctc} ; shift 1
-
-		xctc_defconfig_fbn=$(resolve_buildroot_xctc_defconfig "${xctc_defconfig_fbn:?}" "${main_defconfig_fbn}")
-	fi
+	xctc_defconfig_fbn=${1:-:infer:xctc} ; shift 1 || :
+	xctc_defconfig_fbn=$(resolve_buildroot_xctc_defconfig "${xctc_defconfig_fbn:?}" "${main_defconfig_fbn}")
 
 	local output_selectors=()
 	local output_selector
@@ -187,27 +228,33 @@ function buildroot_all_output_trees_build() { # [ main_defconfig_fbn [ xctc_defc
 
 	! [[ -n ${main_defconfig_fbn} ]] || output_selectors+=( rol main )
 
-	for output_selector in "${output_selectors[@]}" ; do
+	local output_goal_templates=( @load-defconfig@ update-defconfig all )
+	local output_goal_template=
 
-		local output_tree_goal= output_tree_goals=()
+	for output_goal_template in "${output_goal_templates[@]}" ; do
 
-		if [[ "${output_selector:?}" == xctc ]] ; then
-					
-			output_tree_goals+=( "${xctc_defconfig_fbn:?}" )
-		else
-			output_tree_goals+=( "${main_defconfig_fbn:?}" )
-		fi
+		for output_selector in "${output_selectors[@]}" ; do
 
-		output_tree_goals+=( update-defconfig all )
+			if [[ ${output_selector:?} == rol && ${output_goal_template:?} =~ defconfig ]] ; then
 
-		for output_tree_goal in "${output_tree_goals[@]}" ; do
-
-			if [[ ${output_tree_goal} == all || ${output_selector} =~ ^(xctc|main)$ ]] ; then
-
-				xx :
-
-				xx buildroot.sh --output-"${output_selector:?}" "${output_tree_goal:?}"
+				continue # nothing to do: by design rol uses main buildroot config
 			fi
+
+			local output_goal=${output_goal_template:?}
+
+			if [[ ${output_goal:?} == @load-defconfig@ ]] ; then
+
+				if [[ ${output_selector:?} == xctc ]] ; then
+
+					output_goal=${xctc_defconfig_fbn:?}
+				else
+					output_goal=${main_defconfig_fbn:?}
+				fi
+			fi
+
+			xx :
+
+			xx buildroot.sh --output-"${output_selector:?}" "${output_goal:?}"
 		done
 	done
 }
